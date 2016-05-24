6f5eaa071ebe09d665b818564553b835d4a93ca9
Stefan Hinterkoerner <stefan.hinterkoerner@gmail.com>
2016-05-24 16:12:57 +0200

    Merge sort added.

--- Sorting.playground/Contents.swift
+++ Sorting.playground/Contents.swift
@@ -98,4 +98,45 @@ func insertionSort(unsorted:[Int]) -> [Int] {
 }
 insertionSort(array);
 
+/*
+ 
+ MERGE SORT
+ 
+ WORST TIME: O(n log(n))
+ SPACE: O(n)
+ 
+ */
+
+func merge(left left:[Int], right: [Int], result: [Int]) -> [Int] {
+    
+    if left == [] {
+        return result + right;
+    }
+    
+    if right == [] {
+        return result + left;
+    }
+    
+    if left[0] < right[0] {
+        return merge(left: Array(left[1..<left.count]), right: right, result: result + [left[0]]);
+    } else {
+        return merge(left: left, right: Array(right[1..<right.count]), result: result + [right[0]]);
+    }
+    
+}
+func mergeSort(unsorted:[Int]) -> [Int] {
+    var sorted = unsorted;
+    
+    if sorted.count <= 1 {
+        return sorted;
+    }
+    
+    let left = Array(sorted[0..<sorted.count/2]);
+    let right = Array(sorted[sorted.count/2..<sorted.count]);
+    
+    return merge(left: mergeSort(left), right: mergeSort(right), result: []);
+    
+}
+mergeSort(array);
+
 
